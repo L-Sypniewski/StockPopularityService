52bd4be5fd8b88b4fceb44290b7f91372835068d
Łukasz Sypniewski <l.sypniewski@gmail.com>
2020-12-22 18:35:49 +0100

    Added IHtmlReader as a temporary solution

---
+++ StockPopularityCore/Utils/HtmlReader.cs
@@ -0,0 +1,33 @@
+using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace StockPopularityCore.Utils
+{
+    public interface IHtmlDocumentReader // TODO: Remove and use nuget with my custom extensions
+    {
+        HtmlDocument HtmlDocumentFrom(string htmlString);
+    }
+
+    public class HtmlDocumentReader : IHtmlDocumentReader
+    {
+        private readonly ILogger<HtmlDocumentReader> _logger;
+
+
+        public HtmlDocumentReader(ILogger<HtmlDocumentReader>? logger = null)
+        {
+            _logger = logger ?? NullLogger<HtmlDocumentReader>.Instance;
+        }
+
+
+        public HtmlDocument HtmlDocumentFrom(string htmlString)
+        {
+            _logger.LogDebug("Creating HTML document from string");
+            _logger.LogTrace(htmlString);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(htmlString);
+            return htmlDoc;
+        }
+    }
+}
