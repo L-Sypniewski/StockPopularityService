1a70931b25804446c22f9f27e04c032f85307064
Łukasz Sypniewski <l.sypniewski@gmail.com>
2020-12-22 23:52:39 +0100

    Added DI to Azure function

---
+++ StockPopularityFunction/CheckStockPopularitiesTrigger.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using StockPopularityCore.Services.Biznesradar;
+using StockPopularityCore.Utils;
+
+namespace StockPopularityFunction
+{
+    public class CheckStockPopularitiesTrigger
+    {
+        private readonly IHtmlDocumentReader _htmlDocumentReader;
+        private readonly IBiznesradarPopularityService _biznesradarPopularityService;
+
+
+        public CheckStockPopularitiesTrigger(IBiznesradarPopularityService biznesradarPopularityService,
+                                             IHtmlDocumentReader htmlDocumentReader)
+        {
+            _biznesradarPopularityService = biznesradarPopularityService;
+            _htmlDocumentReader = htmlDocumentReader;
+        }
+
+
+        [FunctionName("CheckStockPopularitiesTrigger")]
+        public async Task RunAsync([TimerTrigger("*/40 * * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"CheckStockPopularitiesTrigger executed at: {DateTime.UtcNow}");
+            var page = await _biznesradarPopularityService.StocksPopularity().ToArrayAsync();
+        }
+    }
+}
---
+++ StockPopularityFunction/Startup.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using StockPopularityCore.Services.Biznesradar;
+using StockPopularityCore.Utils;
+
+[assembly: FunctionsStartup(typeof(StockPopularityFunction.Startup))]
+
+namespace StockPopularityFunction
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient<IBiznesradarPopularityService, BiznesradarPopularityService>();
+            builder.Services.AddSingleton<IHtmlDocumentReader, HtmlDocumentReader>();
+        }
+
+
+        public static bool IsDevelopmentEnvironment =>
+            Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT") == "Development";
+    }
+}
