015b7617f86f62b007a9c4f3b5766c7a825fbcb8
Łukasz Sypniewski <l.sypniewski@gmail.com>
2020-12-25 01:19:24 +0100

    BiznesradarPopularityService implements AbstractPopularityService

--- StockPopularityCore/Services/PopularityService/Biznesradar/BiznesradarPopularityService.cs
+++ StockPopularityCore/Services/PopularityService/Biznesradar/BiznesradarPopularityService.cs
@@ -1,86 +1,34 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
 using StockPopularityCore.Model;
 using StockPopularityCore.Utils;
 
 namespace StockPopularityCore.Services.PopularityService
 {
-    public class BiznesradarPopularityService : IBiznesradarPopularityService
+    public class BiznesradarPopularityService : AbstractPopularityService<StockPopularityItem>, IBiznesradarPopularityService
     {
-        private readonly HttpClient _httpClient;
-        private readonly IDateProvider _dateProvider;
-        private readonly ILogger<BiznesradarPopularityService> _logger;
-        private const string Uri = "https://www.biznesradar.pl/symbols-rank/";
-        private readonly IHtmlDocumentReader _documentReader;
-
-
         public BiznesradarPopularityService(HttpClient httpClient, IDateProvider dateProvider,
                                             ILogger<BiznesradarPopularityService> logger)
+            : base(httpClient, dateProvider, logger)
         {
-            _httpClient = httpClient;
-            _dateProvider = dateProvider;
-            _logger = logger;
-            _documentReader = new HtmlDocumentReader();
         }
 
 
-        public async Task<StocksPopularity<StockPopularityItem>> FetchStocksPopularity()
-        {
-            try
-            {
-                var pageSource = await _httpClient.GetStringAsync(Uri);
-                _logger.LogInformation("Fetched site source from url: {uri}", Uri);
-
-                var currentDate = _dateProvider.Now;
-                var tableElements = TableElementsFrom(pageSource);
-                _logger.LogInformation("Fetched table elements from page source");
-
-                var stocksPopularityItems = tableElements.Select(PopularityDataFrom)
-                                                         .Select(tuple => new StockPopularityItem(tuple.stockName, tuple.rank));
-
-                _logger.LogInformation("Created stock popularity items from Biznesradar data");
-
-                return new StocksPopularity<StockPopularityItem>(stocksPopularityItems, currentDate);
-            }
-            catch (Exception exception)
-            {
-                _logger.LogError(
-                    "An exception has been throw when fetching stocks popularity from Biznesradar. Exception message: {message}",
-                    exception.Message);
-                throw;
-            }
-        }
+        protected override string Uri => "https://www.biznesradar.pl/symbols-rank/";
+        protected override string TableXpath => "//table[@class='qTableFull symbol-rank']";
+        protected override string TableRowsXpath => "tr";
+        protected override string WebsiteDisplayName => "Biznesradar";
 
 
-        private IEnumerable<string> TableElementsFrom(string pageSource)
-        {
-            var document = _documentReader.HtmlDocumentFrom(pageSource);
-            var rows = TableRowsFrom(document);
-            return rows.Select(row => row.InnerText.WithoutNewLineChars())
-                       .Where(rowWithoutNewLineChars => !string.IsNullOrWhiteSpace(rowWithoutNewLineChars));
-        }
-
-
-        private static IEnumerable<HtmlNode> TableRowsFrom(HtmlDocument htmlDocument)
-        {
-            var table = htmlDocument.DocumentNode.SelectSingleNode("//table[@class='qTableFull symbol-rank']");
-            return table.SelectNodes("tr").Skip(1);
-        }
-
-
-        private static (int rank, StockName stockName) PopularityDataFrom(string rowString)
+        protected override StockPopularityItem PopularityItemFrom(string rowString)
         {
             var stringElements = rowString.Split(" ").Where(x => x != "").ToArray();
 
             var rank = int.Parse(stringElements.First());
             var stockName = StockNameFrom(stringElements);
 
-            return ( rank, stockName );
+            return new StockPopularityItem(stockName, rank);
         }
 
 
